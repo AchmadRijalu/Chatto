40d6a7dc0eeb51ef8a945113923680286da24eb0
Achmad Rijalu Arianindita <achmad.rijalu@gmail.com>
2024-06-05 09:30:32 +0700

    initial commit

---
+++ Chatto/ChattoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattoApp.swift
+//  Chatto
+//
+//  Created by Achmad Rijalu on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ChattoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
